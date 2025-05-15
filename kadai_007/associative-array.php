0567da3f6e5a270c57fa1824796198679565d41b
mariko uehara <ms0913mp@gmail.com>
2025-05-15 14:52:56 +0900

    Add files via upload

---
+++ kadai_007/associative-array.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html lang ="ja">
+
+<head>
+  <meta charset="UTF-8">
+  <title>kadai_007</title>
+</head>
+
+<body>
+  <p>
+    <pre>
+    <?php
+    $vegetable = ['name' => 'onion', 'price'=> 200, 'weight'=> 160];
+
+    print_r($vegetable);
+    ?>
+    </pre>
+  </p>
+</body>
+
+</html>
